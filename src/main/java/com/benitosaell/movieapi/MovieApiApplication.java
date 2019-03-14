c7288b7a4882fb464dfea6965e154c1cf856534e
Beni <benito-1101@hotmail.com>
2019-03-13 20:34:50 -0600

    Remove security config temp

--- src/main/java/com/benitosaell/movieapi/MovieApiApplication.java
+++ src/main/java/com/benitosaell/movieapi/MovieApiApplication.java
@@ -2,9 +2,8 @@ package com.benitosaell.movieapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class MovieApiApplication {
 
 	public static void main(String[] args) {
