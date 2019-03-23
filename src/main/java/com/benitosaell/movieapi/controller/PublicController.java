9f7e7f1c4c013341917e6cb2a8e6446e64f2864f
Beni <benito-1101@hotmail.com>
2019-03-22 23:32:20 -0600

    Fixs access TO DO: Fixs signIn and signUp

--- src/main/java/com/benitosaell/movieapi/controller/PublicController.java
+++ src/main/java/com/benitosaell/movieapi/controller/PublicController.java
@@ -2,16 +2,13 @@ package com.benitosaell.movieapi.controller;
 
 import java.util.List;
 
-import javax.annotation.Resource;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,50 +27,50 @@ import com.benitosaell.movieapi.service.IUsersService;
 @RestController
 @RequestMapping("/api/publico")
 public class PublicController {
-	@Autowired
-	private IMoviesService serviceMovies;
-	
-	@Autowired
-	private ICommentsService serviceComments;
-	
-	@Autowired
-	private IUsersService serviceUsers;
-	
+
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	//@Resource(name = "authenticationManager")
-	//private AuthenticationManager authManager;
-	
+	@Autowired
+	private IMoviesService serviceMovies;
+
+	@Autowired
+	private ICommentsService serviceComments;
+
+	@Autowired
+	private IUsersService serviceUsers;
+
+	//BCryptPasswordEncoder bCryptPasswordEncoder;
+
 	@GetMapping("/peliculas")
-	private List<Movie> searchAllOrder(){
+	private List<Movie> searchAllOrder() {
 		return serviceMovies.searchAllOrder();
 	}
-	
+
 	@GetMapping("/ver/{id}")
-	private Movie searchMovie(@PathVariable("id") int id){
+	private Movie searchMovie(@PathVariable("id") int id) {
 		return serviceMovies.searchMovie(id);
 	}
-	
+
 	@GetMapping("/comentarios/{idMovie}")
 	private List<Comment> searchComments(@PathVariable("idMovie") int idMovie) {
 		return serviceComments.searchByMovie(idMovie);
 	}
-	
+
 	@PostMapping("/crear")
 	private User insertUser(@RequestBody User user) {
+		//String temp = bCryptPasswordEncoder.encode(user.getPassword());
+		//user.setPassword(temp);
 		serviceUsers.save(user);
-
 		return user;
-	}
+	}	
 	
-	/*@PostMapping("/login")
-	private SecurityContext login(@RequestBody UserLogin user, HttpServletRequest req) {
-		/UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(user.getUsername(),
-				bCryptPasswordEncoder.encode(user.getPassword()));
-		Authentication auth = authManager.authenticate(authReq);
-		SecurityContext sc = SecurityContextHolder.getContext();
-		sc.setAuthentication(auth);
-		// HttpSession session = req.getSession(true);
-		return sc;
-	}*/
+	@PostMapping("/ingresar")
+	private String ingresar(@RequestBody UserLogin user) {
+		//System.out.println("ffffffff");
+		UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(user.getUsername(),
+				  bCryptPasswordEncoder.encode(user.getPassword()));
+		//System.out.println("Data" + user+ "  : "+authReq);
+		return "resultado";
+
+	}
 }
