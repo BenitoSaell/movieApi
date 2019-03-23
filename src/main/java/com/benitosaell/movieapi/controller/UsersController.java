0aa08e894c65ff9212d44fbbac7e7e6cd405c3b5
Beni <benito-1101@hotmail.com>
2019-03-22 23:31:37 -0600

    Remove trash lines

--- src/main/java/com/benitosaell/movieapi/controller/UsersController.java
+++ src/main/java/com/benitosaell/movieapi/controller/UsersController.java
@@ -2,23 +2,12 @@ package com.benitosaell.movieapi.controller;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.benitosaell.movieapi.model.User;
-import com.benitosaell.movieapi.model.UserLogin;
 import com.benitosaell.movieapi.service.IUsersService;
 
 @RestController
@@ -30,7 +19,7 @@ public class UsersController {
 
 	@GetMapping("/users")
 	private List<User> searchAll() {
-		return null;
+		return serviceUsers.searchAll();
 	}	
 }
 	
