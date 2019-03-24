88fedd58a0fea6bf1feb963f1b493a37f7b4bf6e
Beni <benito-1101@hotmail.com>
2019-03-23 18:20:05 -0600

    Adds @Async

--- src/main/java/com/benitosaell/movieapi/service/IProfilesService.java
+++ src/main/java/com/benitosaell/movieapi/service/IProfilesService.java
@@ -1,7 +1,10 @@
 package com.benitosaell.movieapi.service;
 
+import org.springframework.scheduling.annotation.Async;
+
 import com.benitosaell.movieapi.model.Profile;
 
 public interface IProfilesService {
+	@Async("asyncExecutor")
 	void save(Profile profile);
 }
--- src/main/java/com/benitosaell/movieapi/service/IUsersService.java
+++ src/main/java/com/benitosaell/movieapi/service/IUsersService.java
@@ -2,10 +2,16 @@ package com.benitosaell.movieapi.service;
 
 import java.util.List;
 
+import org.springframework.scheduling.annotation.Async;
+
 import com.benitosaell.movieapi.model.User;
+import com.benitosaell.movieapi.model.UserLogin;
 
 public interface IUsersService {
 	List<User> searchAll();
+	@Async("asyncExecutor")
 	void save(User user);
 	User searchUserByUsername(String username);
+	@Async("asyncExecutor")
+	String login(UserLogin user);
 }
